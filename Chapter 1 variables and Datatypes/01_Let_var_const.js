1cbe8522d86163548a04b4cfec85fdd351eb31ad
Jayesh Jadhav <114849888+Jayesh-2981@users.noreply.github.com>
2024-09-29 18:55:56 +0530

    Add files via upload

---
+++ Chapter 1 variables and Datatypes/01_Let_var_const.js
@@ -0,0 +1,16 @@
+console.log("Let, Const, Var");
+var a = 45;
+var a = "few"
+let b = "Jayesh"
+const author = "Jayesh";
+// author = 4; //Thrws an error
+b = 4;
+const jerry = 0;
+let c = null;
+let d = undefined
+
+{
+    let b = 'this'
+    console.log(b);
+}
+console.log(b);
---
+++ Chapter 1 variables and Datatypes/02_PrimitiveDataTypes_and_Objects.js
@@ -0,0 +1,19 @@
+console.log("7 Primitive data types");
+let a = null //null
+let b = 3763289 //number
+let c = true //boolean
+let d = BigInt("346324") //BigInt
+let e = "Jayesh" //String
+let f = Symbol("I am a symbol") //symbol
+let g //undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof g);
+console.log();
+console.log("Object in js or non - primitive data types in js");
+const item = {
+    "Jayesh": true,
+    "fjd": false,
+    "jd": 23,
+    "Akshay": undefined
+}
+console.log(item["jd"])
